c582254f6d941fe6500cd6e613489300978bbac6
Ian Eduardo <edu922077@gmail.com>
2025-09-10 22:19:40 -0300

    Create ex-02.js

---
+++ exercicios/ex-02.js
@@ -0,0 +1,11 @@
+//exercicio 2
+
+let num1 = input.questionInt ("Digite um número: ")
+let num2 = input.questionInt ("Digite um número: ")
+
+    if (num1<num2) {
+    console.log("O maior valor é " + (num2+=5));
+} 
+    else {
+        console.log ("O maior número é " + (num1+=5));
+    }
