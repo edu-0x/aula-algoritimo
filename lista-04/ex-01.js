9bd4bc5ea184c4651269b50d8464b4e537f1e54f
Ian Eduardo <edu922077@gmail.com>
2025-09-17 21:53:09 -0300

    Update ex-01.js

--- lista-04/ex-01.js
+++ lista-04/ex-01.js
@@ -3,9 +3,9 @@
 let num1 = input.questionInt("Digite um número: ")
 let num2 = input.questionInt("Digite um número: ")
 
-if (num1>num2) {
-    console.log("O maior número é: " + num1);
+if (num1 < num2) {
+    console.log("O menor número é: " + num1);
 }
 else {
-    console.log("O maior número é: " + num2)
+    console.log("O menor número é: " + num2)
 }
